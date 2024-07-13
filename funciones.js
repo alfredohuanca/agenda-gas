645c0a8a52355a74fa6aa3e8f87bd5992ece7cb8
AlfredoHuanca <alfred.huanca@gmail.com>
2024-07-12 23:04:29 -0500

    curso GAS-avanzado

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function doGet()
+{
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+
+}
+
+function obtenerDatosHTML(nombre)
+{
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+
+
+}
